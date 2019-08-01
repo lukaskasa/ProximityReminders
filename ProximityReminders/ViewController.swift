f06dc17833840a5adfa08aff139e60a18a490369
Lukas Kasakaitis <lukas.kasakaitis@icloud.com>
2019-08-01 17:41:47 +0200

    Initial Commit

---
+++ ProximityReminders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProximityReminders
+//
+//  Created by Lukas Kasakaitis on 01.08.19.
+//  Copyright © 2019 Lukas Kasakaitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
